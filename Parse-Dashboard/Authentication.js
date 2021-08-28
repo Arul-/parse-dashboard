e3896af0161b3bd09028f5c8f47926c5f8db46c4
Arul <arul@luracast.com>
2021-08-28 23:34:00 +0800

    V2.1.4

--- Parse-Dashboard/Authentication.js
+++ Parse-Dashboard/Authentication.js
@@ -74,8 +74,8 @@ function initialize(app, options) {
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false, // if true only transmit cookie over https
-      httpOnly: false, // if true prevent client side JS from reading the cookie
+      secure: true, // if true only transmit cookie over https
+      httpOnly: true, // if true prevent client side JS from reading the cookie
       maxAge: 1000 * 60 * 10 // session max age in miliseconds
     }
   }))
